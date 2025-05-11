39285bdb196849b479028a9ba1b418f663235276
Jutinut Ratanamongkonkul-6609650228 <145243765+Jutinut@users.noreply.github.com>
2025-05-11 10:43:29 +0700

    fix path build loginregister

---
+++ frontend/login/script.js
@@ -0,0 +1,10 @@
+function showRegister() {
+    document.getElementById("loginBox").classList.add("hidden");
+    document.getElementById("registerBox").classList.remove("hidden");
+  }
+  
+  function showLogin() {
+    document.getElementById("registerBox").classList.add("hidden");
+    document.getElementById("loginBox").classList.remove("hidden");
+  }
+  
