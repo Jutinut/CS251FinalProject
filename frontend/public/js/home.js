adc7a73adf42fe4fccfef03f8654442fefe3b944
Suttipot Prathoumthong - 6609650699 <suttipot.pra@dome.tu.ac.th>
2025-05-16 05:51:18 +0700

    create model foreach home

--- frontend/public/js/home.js
+++ frontend/public/js/home.js
@@ -24,3 +24,75 @@
       resultBox.style.color = "crimson";
     }
   });
+
+async function fetchRequests() {
+    try {
+        const trackingInput = document.getElementById("tracking-number");
+        const CustID = trackingInput.value.trim();
+        if (!CustID) {
+            throw new Error("Customer not found orders.");
+        }
+        const queryURL = `http://localhost:3000/order/${CustID}`;
+        console.log(`Query URL: ${queryURL}`);
+
+        const response = await fetch(queryURL);
+        if (!response.ok) throw new Error("Failed to fetch data");
+
+        let data = await response.json();
+        console.log("Data fetched:", data);
+
+        const complaintContainer = document.getElementById("trackbox");
+        if (!complaintContainer) throw new Error("Complaint container element not found");
+
+        complaintContainer.innerHTML = "";
+
+        if (!Array.isArray(data)) {
+            data = [data];
+        }
+
+        if (data.length === 0) {
+            complaintContainer.innerHTML = "<p>No orders found</p>";
+            return;
+        }
+        
+        const sortedData = data.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+
+        sortedData.forEach((request) => {
+            const item = document.createElement("div");
+            item.classList.add("track");
+
+            item.innerHTML = `
+                <table>
+          <thead>
+            <tr>
+              <th scope="col">ParcelID</th>
+              <th scope="col">Phone</th>
+              <th scope="col">Name</th>
+              <th scope="col">City</th>
+              <th scope="col">Address</th>
+              <th scope="col">Weight</th>
+              <th scope="col">Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">${request.ParcelID}</th>
+              <td>${request.Phone}</td>
+              <td>${request.Name}</td>
+              <td>${request.City}</td>
+              <td>${request.Address}</td>
+              <td>${request.Weight}</td>
+              <td>${request.Status}</td>
+            </tr>
+          </tbody>
+        </table>`;
+            complaintContainer.appendChild(item);
+        });
+    } catch (error) {
+        console.error("Error fetching requests:", error);
+        const complaintContainer = document.getElementById("tracking-container");
+        if (complaintContainer) {
+            complaintContainer.innerHTML = `<p>Error loading requests. Please try again later.</p>`;
+        }
+    }
+}
